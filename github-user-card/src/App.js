e161dc62d1ddf9d4ed2c5b58427593ebbe77e3e9
Temitope-Akinsoto <takinsoto@gmail.com>
2019-10-01 09:36:28 -0700

    Initialize Project files using create-react-app

---
+++ github-user-card/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <p>Welcome to Git Hub User Card</p>
+    </div>
+  );
+}
+
+export default App;
